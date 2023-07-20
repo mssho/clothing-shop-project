278809c0e78c007d4565e71ec5832bb57741b79c
Sadegh Shokoohian <shokoohianm@yahoo.com>
2023-07-20 07:51:06 +0330

    add admin configurations for each model

--- clothing_shop/orders/admin.py
+++ clothing_shop/orders/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Address, Order, OrderItem
 # Register your models here.
+admin.site.register(Address)
+admin.site.register(Order)
+admin.site.register(OrderItem)
