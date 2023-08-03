5046022fc1184f71d566cf6d16e3a129aa769355
Sadegh Shokoohian <shokoohianm@yahoo.com>
2023-08-03 03:51:32 +0330

    feat: add User class as AbstractBaseUser

--- clothing_shop/accounts/models.py
+++ clothing_shop/accounts/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-
-# Create your models here.
-
-class Customer(models.Model):
+from django.contrib.auth.models import AbstractBaseUser
+from phonenumber_field.modelfields import PhoneNumberField
+class User(AbstractBaseUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=20)
+    phone_number = PhoneNumberField(unique=True)
     # ToDo: user authentication and abstractBaseUser implementations are needed
+    USERNAME_FIELD = 'phone_number'
+    def __str__(self) -> str:
+        return str(self.phone_number)
